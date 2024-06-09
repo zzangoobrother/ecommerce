66f1074397efff0188773a6c2cafe8819083a093
choiseonkang <com1368@naver.com>
2024-06-09 23:56:48 +0900

    init : storage, domain, api 멀티 모듈 기본 세팅

--- ecommerce-api/src/main/java/com/example/EcommerceApiApplication.java
+++ ecommerce-api/src/main/java/com/example/EcommerceApiApplication.java
@@ -1,13 +1,12 @@
-package com.example.ecommerce;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EcommerceApplication.class, args);
+        SpringApplication.run(EcommerceApiApplication.class, args);
     }
-
 }
