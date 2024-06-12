850bc35c15dc32610005625d74e50e5e1010951f
choiseonkang <com1368@naver.com>
2024-06-12 21:17:48 +0900

    init : gateway, member-api 멀티 모듈 기본 세팅

---
+++ gateway/src/main/java/com/example/GateWayApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
