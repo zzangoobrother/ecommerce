ace4e5950087d19ce2d1ad779b71021a46710dfd
choiseonkang <com1368@naver.com>
2024-07-17 01:02:22 +0900

    feat : redis 상품 재고 확인 구현 1

---
+++ storage/redis/src/main/java/com/example/repository/RedisCountRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.example.repository;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RedisCountRepositoryImpl implements RedisCountRepository {
+
+    private final RedisTemplate<String, Long> redisTemplate;
+
+    public RedisCountRepositoryImpl(RedisTemplate<String, Long> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void add(String key, Long value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    @Override
+    public Long decrement(String key, Long value) {
+        return redisTemplate.opsForValue().decrement(key, value);
+    }
+
+    @Override
+    public Long get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+}
